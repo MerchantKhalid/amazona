17a98b096a6e9dab6d10891f217abd3b3b0a6d59
Mohammad Khalid Hasan <rabbikhalidhasan@gmail.com>
2023-04-15 11:20:58 +0200

    used reducer

--- amazona_client/src/Pages/Home/Home.js
+++ amazona_client/src/Pages/Home/Home.js
@@ -1,19 +1,60 @@
-import React from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import data from '../../data.js';
 import HomePageCard from '../HomaPageCard/HomePageCard.js';
 import { Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import logger from 'use-reducer-logger';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, products: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
 const Home = () => {
+  // const [products, setProducts] = useState([]);
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    products: [],
+    loading: true,
+    error: '',
+  });
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const result = await axios.get('/api/products');
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (error) {
+        dispatch({ type: 'FETCH_FAIL', payload: error.message });
+      }
+
+      // setProducts(result.data);
+    };
+    fetchData();
+  }, []);
   return (
     <div>
       <h2>Featured Products</h2>
       <Container>
         <Row>
           <Col xs={4} sm={6} lg={4}>
-            {data.products.map((pd) => (
-              <HomePageCard key={pd.slug} product={pd}></HomePageCard>
-            ))}
+            {loading ? (
+              <div>Loading...</div>
+            ) : error ? (
+              <div>{error}</div>
+            ) : (
+              products.map((pd) => (
+                <HomePageCard key={pd.slug} product={pd}></HomePageCard>
+              ))
+            )}
           </Col>
         </Row>
       </Container>
---
+++ amazona_server/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import data from './data.js';
+
+const app = express();
+
+const port = process.env.PORT || 6000;
+
+app.get('/api/products', (req, res) => {
+  res.send(data.products);
+});
+
+app.listen(port, () => {
+  console.log(`server running at http://localhost:${port}`);
+});
