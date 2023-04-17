aff10104f4d9f3d77a9eb91546148e0aab144aae
Mohammad Khalid Hasan <rabbikhalidhasan@gmail.com>
2023-04-17 18:54:10 +0200

    cart implemented

--- amazona_client/src/Pages/HomaPageCard/HomePageCard.js
+++ amazona_client/src/Pages/HomaPageCard/HomePageCard.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 import Rating from './Rating';
+import axios from 'axios';
+import { Store } from '../../Context/Store';
 
 const HomePageCard = ({ product }) => {
   const { name, image, slug, price, rating, numReviews, description } = product;
+
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const {
+    cart: { cartItems },
+  } = state;
+
+  const addToCartHandler = async (item) => {
+    const existItems = cartItems.find((x) => x._id === product._id);
+    const quantity = existItems ? existItems.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${item._id}`);
+    if (data.countInStock < quantity) {
+      window.alert('Sorry this product is out of stock');
+      return;
+    }
+    ctxDispatch({
+      type: 'CART_ADD_ITEM',
+      payload: { ...item, quantity },
+    });
+  };
+
   return (
     <div>
       <Card>
@@ -22,7 +44,15 @@ const HomePageCard = ({ product }) => {
             numReviews={numReviews}
           ></Rating>
           <Card.Text>{description}</Card.Text>
-          <Button variant="warning">Add To Cart</Button>
+          {product.countInStock === 0 ? (
+            <Button disabled variant="light">
+              Out of stock
+            </Button>
+          ) : (
+            <Button onClick={() => addToCartHandler(product)} variant="warning">
+              Add To Cart
+            </Button>
+          )}
         </Card.Body>
       </Card>
     </div>
